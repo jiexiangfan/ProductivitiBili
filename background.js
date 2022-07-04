65feaf4e92f136ac9e7883c20398f4e1f19b6d4d
Jie F <51945335+jiexiangfan@users.noreply.github.com>
2022-07-04 22:58:49 +1000

    Basic feature of blocking video suggestion on bilibili.com (#1)

---
+++ background.js
@@ -0,0 +1,8 @@
+// Background script aka service worker for the extension
+
+let color = "#3aa757";
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log("Default background color set to %cgreen", `color: ${color}`);
+});
